af9af3745ac3bc02415589e2f6bda487d08da78d
sallerli1 <sallerli100@gmail.com>
2018-05-20 19:52:45 +0800

    testing

--- src/components/submit.js
+++ src/components/submit.js
@@ -6,12 +6,13 @@ export default new Component({
     selector: '#submit',
     template: `<a bind-touchstart='submit'>{{text}}</a>`,
     data: {
-        text: "提交"
+        text: "提交",
+        url: 'http://yiban.xaut.edu.cn/weixin/admin/web/common/question2'
     },
     methods: {
         submit() {
             if (data.percent>=100) {
-                post('http://www.bing.com', data.answers).then((res) => {
+                post(this.data.url, data.answers).then((res) => {
                     if (res.success) {
                         alert('提交成功')
                     } else {
@@ -22,8 +23,7 @@ export default new Component({
                 })
             } else {
                 alert('请填写全部问卷')
-            }
-            
+            }         
         }
     }
 })
